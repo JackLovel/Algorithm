7c5552ffcf6a2c671d373a917309569f74c95ac2
WangCC <gcc_linux@foxmail.com>
2018-12-26 15:43:17 +0800

    Update Selection.java

--- Java/Selection.java
+++ Java/Selection.java
@@ -1,49 +1,13 @@
-/******************************************************************************
- *  Compilation:  javac Selection.java
- *  Execution:    java  Selection < input.txt
- *  Dependencies: StdOut.java StdIn.java
- *  Data files:   https://algs4.cs.princeton.edu/21elementary/tiny.txt
- *                https://algs4.cs.princeton.edu/21elementary/words3.txt
- *
- *  Sorts a sequence of strings from standard input using selection sort.
- *
- *  % more tiny.txt
- *  S O R T E X A M P L E
- *
- *  % java Selection < tiny.txt
- *  A E E L M O P R S T X                 [ one string per line ]
- *
- *  % more words3.txt
- *  bed bug dad yes zoo ... all bad yet
- *
- *  % java Selection < words3.txt
- *  all bad bed bug dad ... yes yet zoo    [ one string per line ]
- *
- ******************************************************************************/
 package Aglo4;
 
 import edu.princeton.cs.algs4.*;
 import java.util.Comparator;
 
-/**
- *  The {@code Selection} class provides static methods for sorting an
- *  array using selection sort.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/21elementary">Section 2.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
+// code copy from algs4
 public class Selection {
 
-    // This class should not be instantiated.
     private Selection() { }
 
-    /**
-     * Rearranges the array in ascending order, using the natural order.
-     * @param a the array to be sorted
-     */
     public static void sort(Comparable[] a) {
         int n = a.length;
         for (int i = 0; i < n; i++) {
@@ -57,11 +21,6 @@ public class Selection {
         assert isSorted(a);
     }
 
-    /**
-     * Rearranges the array in ascending order, using a comparator.
-     * @param a the array
-     * @param comparator the comparator specifying the order
-     */
     public static void sort(Object[] a, Comparator comparator) {
         int n = a.length;
         for (int i = 0; i < n; i++) {
@@ -75,17 +34,10 @@ public class Selection {
         assert isSorted(a, comparator);
     }
 
-
-    /***************************************************************************
-     *  Helper sorting functions.
-     ***************************************************************************/
-
-    // is v < w ?
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
 
-    // is v < w ?
     private static boolean less(Comparator comparator, Object v, Object w) {
         return comparator.compare(v, w) < 0;
     }
@@ -97,11 +49,6 @@ public class Selection {
         a[i] = a[j];
         a[j] = swap;
     }
-
-
-    /***************************************************************************
-     *  Check if array is sorted - useful for debugging.
-     ***************************************************************************/
 
     // is the array a[] sorted?
     private static boolean isSorted(Comparable[] a) {
@@ -127,8 +74,6 @@ public class Selection {
         return true;
     }
 
-
-
     // print array to standard output
     private static void show(String text, Comparable[] a) {
         System.out.print(text + ": ");
@@ -138,12 +83,6 @@ public class Selection {
         System.out.println();
     }
 
-    /**
-     * Reads in a sequence of strings from standard input; selection sorts them; 
-     * and prints them to standard output in ascending order. 
-     *
-     * @param args the command-line arguments
-     */
     public static void main(String[] args) {
         String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
         show("before", a);
